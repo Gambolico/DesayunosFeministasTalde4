254abd426bac9902677dbf37ef6fc1e9ebe67989
Gambolico <mikelgamb@gmail.com>
2020-12-14 12:25:07 +0100

    creacion vistas

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -16,3 +16,57 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//* Vista padre
+
+Route::get('/parent', function () {
+    return view('parent');
+});
+
+//* Vista login
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+//* Vista register
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+//* Vista inicio juego
+
+Route::get('/iniciojuego', function () {
+    return view('inicioJuego');
+});
+
+//* Vista elegir juego
+
+Route::get('/elegirjuego', function () {
+    return view('elegirJuego');
+});
+
+//* Vista elegir modo
+
+Route::get('/elegirmodo', function () {
+    return view('elegirModo');
+});
+
+//* Vista parejas
+
+Route::get('/pareja', function () {
+    return view('pareja');
+});
+
+//* Vista adivina
+
+Route::get('/adivina', function () {
+    return view('adivina');
+});
+
+//* Vista coleccion
+
+Route::get('/coleccion', function () {
+    return view('coleccion');
+});
