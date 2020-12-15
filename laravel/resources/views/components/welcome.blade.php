6bda7506d3c404b45879e99bed6d950d4cc46fbe
MarkelMiranda <markel.miranda1@gmail.com>
2020-12-15 08:47:41 +0100

    Botonones - tamaños

--- laravel/resources/views/components/welcome.blade.php
+++ laravel/resources/views/components/welcome.blade.php
@@ -7,13 +7,17 @@
         .boton{
             background-color:#261343;
             color:white;
-            
+            padding-top:30px;
+            padding-bottom:35px;
+            padding-left:70px;
+            padding-right:70px;
+            font-size:25px;
+            border-radius:8px;
         }
     </style>
 
     <div>
         <button type="button" class="btn boton">EMPEZAR</button>
-        <button type="button"  class="btn boton">MUJERES</button>
         <button type="button"  class="btn boton">COMO JUGAR</button>
     </div>
 @stop
