d38727f48f05fbc3cfa551eb3e076d0983be15e1
Oihane Arranz <oihanefpweb@gmail.com>
2020-12-15 12:10:09 +0100

    Modelos y añadida la ruta en los controladores

---
+++ laravel/app/Models/Usuarios.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Usuarios extends Model
+{
+        //La tabla que vamos a usar
+        protected $table = 'usuarios';
+}
