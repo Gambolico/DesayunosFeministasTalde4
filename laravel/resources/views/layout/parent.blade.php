79c692157e78b8c1bc6a4d175d31a93722079d0d
KaietOrza <kaiet.orza@gmail.com>
2020-12-15 09:23:38 +0100

    Login

--- laravel/resources/views/layout/parent.blade.php
+++ laravel/resources/views/layout/parent.blade.php
@@ -6,6 +6,9 @@
         
         <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+            <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+            <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+            @yield('css')
         <style>
         .navbar {
             background-color:#261343;
