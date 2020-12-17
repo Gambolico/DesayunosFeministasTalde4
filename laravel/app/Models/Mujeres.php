6e268766c0f670f767ff4b669742dc0bfa8a62aa
Oihane Arranz <oihanefpweb@gmail.com>
2020-12-17 10:18:11 +0100

    dd

--- laravel/app/Models/Mujeres.php
+++ laravel/app/Models/Mujeres.php
@@ -11,8 +11,9 @@ class Mujeres extends Model
 
         public function getMujeresInf()
         {   
-            $mujeres = Mujeres::select('Mujeres_Id', 'Nombre','Apellido','Fecha_Nacimiento','Fecha_Muerte','Lore_Esp','')
-            ->join('categories', 'users.idUser', '=', 'categories.user_id')
+            $mujeres = Mujeres::select('Mujeres_Id', 'Nombre','Apellido','Fecha_Nacimiento','Fecha_Muerte','Lore_Esp','Zona_Geografica','continentes.Nombre_Continente','Img_Ruta','Enlace_Referencia','ambitos.Nombre_Ambito')
+            ->join('ambitos', 'ambitos.Id_Ambito', '=', 'mujeres.Ambito_Id') ->join('continentes','continentes.Id_Continente', '=', 'mujeres.Continente_Id')
+            ->orderByRaw('Mujeres_Id asc')
             ->get();
             return $mujeres;
         }
