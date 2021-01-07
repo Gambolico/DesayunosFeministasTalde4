b8a810897ce9d68d809a61d193a55cfa04b739e0
KaietOrza <kaiet.orza@gmail.com>
2021-01-07 10:01:58 +0100

    fotos

--- laravel/resources/views/components/pareja.blade.php
+++ laravel/resources/views/components/pareja.blade.php
@@ -32,12 +32,12 @@
         @foreach ($mujeres as $obj)
             <li class="card" type="{{$obj->Mujeres_Id}}">
                 <i class="">
-                <img src="../images/fotos de mujeres/{{$obj->Img_Ruta}}">
+                <img src="../images/fotos_de_mujeres/{{$obj->Img_Ruta}}">
                 </i>
             </li>
             <li class="card" type="{{$obj->Mujeres_Id}}">
                 <i class="">
-                <img src="../images/fotos de mujeres/{{$obj->Img_Ruta}}">
+                <img src="../images/fotos_de_mujeres/{{$obj->Img_Ruta}}">
                 </i>
             </li>
         @endforeach
@@ -46,19 +46,18 @@
     </div>
 
     <div id="popup1" class="overlay">
-            <div class="popup">
-                <a class="close" href=# >×</a>
-                <h1>ENHORABUENA</h1>
-                <div class="content-1">
-                    Has conseguido acabar el juego en
-                </div>
-                <div class="content-2">
-                    <p><span id=finalMove> </span> movimientos y <span id=totalTime> </span> </p>
-                </div>
-                <a class="underlineHover" href="{{ route('empezarParejas', ['modo'=>$modo])}}">Desbloquear las mujeres de esta partida</a>
+        <div class="popup">
+            <a class="close" href=# >×</a>
+            <h1>ENHORABUENA</h1>
+            <div class="content-1">
+                Has conseguido acabar el juego en
             </div>
-            
-        </div>
+            <div class="content-2">
+                <p><span id=finalMove> </span> movimientos y <span id=totalTime> </span> </p>
+            </div>
+            <a class="underlineHover" href="{{ route('empezarParejas', ['modo'=>$modo])}}">Desbloquear las mujeres de esta partida</a>
+        </div>            
+    </div>
         
     <!-- JS -->
     <script src="{{ asset('JS/Parejas.js') }}"></script>
