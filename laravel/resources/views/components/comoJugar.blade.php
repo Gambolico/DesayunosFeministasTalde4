07173ffee645142f1919d48c69d832bb61958eaf
Gambolico <mikelgamb@gmail.com>
2021-01-20 10:22:19 +0100

    añadir carousel

--- laravel/resources/views/components/comoJugar.blade.php
+++ laravel/resources/views/components/comoJugar.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Como jugar')
 
 @section('content')
-<script>
+{{-- <script>
         var slideIndex = 1;
       showSlides(slideIndex);
 
@@ -67,6 +67,45 @@
         <span class="dot" onclick="currentSlide(1)"></span>
         <span class="dot" onclick="currentSlide(2)"></span>
         <span class="dot" onclick="currentSlide(3)"></span>
+    </div> --}}
+
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="images/fotos_de_mujeres/gatete.gif" alt="First slide">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Prueba</h5>
+        <p>eyy</p>
+      </div>
     </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="" alt="Second slide">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Prueba</h5>
+          <p>eyy</p>
+        </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Third slide">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Prueba</h5>
+          <p>eyy</p>
+        </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
     
 @stop
